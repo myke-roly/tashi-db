b7dea7f4944844d8156130708c702401d8011e11
mike-bds <rchallapa@ext.bancodelsol.com>
2022-08-21 00:34:41 -0300

    Delete publish api

--- src/api/publish/routes/publish.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * publish router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::publish.publish');
--- src/api/publish/services/publish.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * publish service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::publish.publish');
